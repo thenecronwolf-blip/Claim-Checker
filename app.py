8ffaf15ea14fc6ae6927301416fb7c2e51cdb8a8
thenecronwolf-blip <thenecronwolf@gmail.com>
2025-12-14 22:07:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def about():
 def docs():
     return render_template('docs.html')
 
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
 @app.route('/health')
 def health():
     return "OK", 200
